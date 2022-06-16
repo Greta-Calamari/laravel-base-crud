c13d3cb2a6a97ae7cb4a482f84d0cd98318a7e75
ladykakki <greta_calamari@hotmail.it>
2022-06-16 14:16:40 +0200

    funziona tutto ora creo la pagina 'edit'

--- resources/views/Comics/index.blade.php
+++ resources/views/Comics/index.blade.php
@@ -11,13 +11,29 @@
                 @foreach ($comics as $comic)
                 <div class="col">
                     <img src="{{$comic->thumb}}" alt="">
-                    <p>
-                        {{$comic->id}}
+                    <p>{{$comic->title}}</p>
+                    <p>{{$comic->description}}</p>
+                    <p>{{$comic->price}}</p>
+                    <p>{{$comic->series}}</p>
+                    <p>{{$comic->sale_date}}</p>
+                    <p>{{$comic->type}}</p>
+                    <a href="{{route('comics.show',$comic->id)}}" class="btn btn-success">Visualizza</a>
+                    <a href="{{route('comics.edit',$comic->id)}}" class="btn btn-warning">Modifica</a>
+                    <form action="{{route('comics.destroy',$comic->id)}}" method="post">
+                        @csrf
+                        @method('delete')
+                        <button type="submit" class="btn btn-danger">Cancella</button>
+                      
+                    </form>
 
-                    </p>
-                    <p>
-                        {{$comic->title}}
-                    </p>
+
+
+
+
+                    
+                        
+
+                    
 
                 </div>
                     
